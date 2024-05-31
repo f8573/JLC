cb52ac302952e5392e4e0c5fbd17e5e2157ad84d
James Faul <faul8573@stthomas.edu>
2024-05-31 05:02:59 -0500

    start on root finding

---
+++ src/polynomial/RationalPolynomial.java
@@ -0,0 +1,17 @@
+package polynomial;
+
+import number.Rational;
+
+public class RationalPolynomial {
+    //do like everything else ig
+    Rational[] coefficients;
+
+    public RationalPolynomial(Rational... nums) {
+        coefficients = nums;
+    }
+
+    public Rational[] rationalRoots() {
+        //TODO: implement
+        return null;
+    }
+}
