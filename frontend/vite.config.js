bef6906dcdea9a796a56be22edf31cad9c36637b
f8573 <faul8573@stthomas.edu>
2026-02-07 02:53:54 +0000

    add lambdacompute.org as an allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: ['lambdacompute.org'],
     proxy: {
       '/api': 'http://localhost:8080'
     }
